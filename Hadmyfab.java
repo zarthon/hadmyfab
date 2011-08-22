4c87893a4cc18e71f6a12d5b41d3e05f080b3cff
Mohit Kothari <mohitrajkothari@gmail.com>
2011-08-22 19:37:56 +0530

    Added code to insert USERS table data

--- Hadmyfab.java
+++ Hadmyfab.java
@@ -1,6 +1,7 @@
 
 import java.sql.*;
-
+import java.util.*;
+import java.lang.Integer;
 /**
  * This is a sample application has basically 2 usage:-
  * 1) Demonstrate the use of jdbc driver for mysql by connecting to server, droping tables and creating tables and also
@@ -17,6 +18,16 @@ public class Hadmyfab
     public static final String USERNAME = "haduser";
     public static final String PASSWORD = "pass";
     public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+    
+    //Sample Data for insertion
+    public static Hashtable<String,Integer> UserId = new Hashtable<String,Integer>();
+    static{UserId.put("mohit",1);UserId.put("nikhil",2);UserId.put("naman",3);UserId.put("maullik",4);UserId.put("kedar",5);};
+    
+    public static Hashtable<String,String> Users = new Hashtable<String,String>();
+    static{Users.put("mohit","asda");Users.put("nikhil","sadf");Users.put("naman", "qweos");Users.put("maullik", "werqw");Users.put("kedar", "394iurw");};
+    
+    public static Hashtable<String,String> UserProfile = new Hashtable<String,String>();
+    static{UserProfile.put("mohit", "Mohit Kothari 23 Single");UserProfile.put("nikhil","Nikhil Marathe 32 Single");UserProfile.put("naman","Naman Muley 20 Single");UserProfile.put("maullik","Maullik Padia 20 Single");UserProfile.put("kedar", "Kedar Bhatt 25 Single");}; 
     
     private static void init()throws Exception{	
     	Class.forName (DRIVER_CLASS).newInstance ();
@@ -57,11 +68,11 @@ public class Hadmyfab
     	Statement create = null;
     	create = CONN.createStatement();
     	
-    	String users = "CREATE TABLE USERS (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, username varchar(40), password varchar(40))";
+    	String users = "CREATE TABLE USERS (id INT UNSIGNED PRIMARY KEY, username varchar(40), password varchar(40))";
     	String userProfile = "CREATE TABLE USERPROFILE (user_id INT REFERENCES USERS(id), first_name varchar(40), last_name varchar(40), age INT, relation varchar(10))";
     	String friends = "CREATE TABLE FRIENDS (user_id INT REFERENCES USERS(id), friend_id INT REFERENCES USERS(id), PRIMARY KEY(user_id,friend_id))";
-    	String wallPosts = "CREATE TABLE WALLPOSTS (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, user_id INT REFERENCES USERS(id), body VARCHAR(200), timestamp TIMESTAMP )";
-    	String comments = "CREATE TABLE COMMENTS (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, user_id INT REFERENCES USERS(id), wall_id INT REFERENCES WALLPOSTS(id), body VARCHAR(200), timestamp TIMESTAMP )";
+    	String wallPosts = "CREATE TABLE WALLPOSTS (id INT UNSIGNED PRIMARY KEY, user_id INT REFERENCES USERS(id), body VARCHAR(200), timestamp TIMESTAMP )";
+    	String comments = "CREATE TABLE COMMENTS (id INT UNSIGNED PRIMARY KEY, user_id INT REFERENCES USERS(id), wall_id INT REFERENCES WALLPOSTS(id), body VARCHAR(200), timestamp TIMESTAMP )";
     	try{
     		create.executeUpdate(users);
     		create.executeUpdate(userProfile);
@@ -78,10 +89,30 @@ public class Hadmyfab
     
     public static void insertData() throws SQLException{
     		PreparedStatement insert = null;
-    		String insertUsers = "INSERT INTO USERS(username, password) VALUES(?,?)"
+    		String insertUsers = "INSERT INTO USERS(id,username, password) VALUES(?,?,?)";
+    		String insertUserProfile = "INSERT INTO USERPROFILE(user_id, first_name, last_name, age, relation) VALUES(?,?,?,?,?)";
     		try{
-    			insert = CONN.prepareStatement("")
+    			insert = CONN.prepareStatement(insertUsers);
+    			Enumeration<String> user = Users.keys();
+    			System.out.println(Users.isEmpty());
+    			while(user.hasMoreElements()){
+    				String temp = user.nextElement();
+    				insert.setString(2, temp);
+    				insert.setString(3,Users.get(temp));
+    				insert.setString(1,UserId.get(temp).toString());
+    				insert.execute();
+    			}
+    			insert = CONN.prepareStatement(insertUserProfile);
     			
+    		}
+    		catch(SQLException ex){
+    			//Reverse the statements as something went wrong
+    			CONN.rollback();
+    			throw ex;
+    		}
+    		finally{
+    			if(insert != null)
+    				insert.close();
     		}
     	
     }
@@ -91,6 +122,7 @@ public class Hadmyfab
             init();
             dropTables();
             createTables();
+            insertData();
         }
         catch (Exception e)
         {
