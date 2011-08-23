2f2f862bbe8fb0a8d0d8d9e42f6f3879a3952653
Mohit Kothari <mohitrajkothari@gmail.com>
2011-08-23 20:24:56 +0530

    Created new file for mapred job

--- Hadmyfab.java
+++ Hadmyfab.java
@@ -1,7 +1,5 @@
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,29 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.StringTokenizer;
 
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.lib.LongSumReducer;
-import org.apache.hadoop.mapred.lib.db.DBConfiguration;
-import org.apache.hadoop.mapred.lib.db.DBInputFormat;
-import org.apache.hadoop.mapred.lib.db.DBOutputFormat;
-import org.apache.hadoop.mapred.lib.db.DBWritable;
-import org.apache.hadoop.util.StringUtils;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 
 /**
  * This is a sample application has basically 2 usage:-
@@ -92,6 +68,7 @@ public class Hadmyfab
     		drop.executeUpdate("DROP TABLE IF EXISTS WALLPOSTS");
     		drop.executeUpdate("DROP TABLE IF EXISTS COMMENTS");
     		drop.executeUpdate("DROP TABLE IF EXISTS AGGREGATE");
+    		drop.executeUpdate("DROP TABLE IF EXISTS RESULT");
     		System.out.println("Tables Dropped");
     	}
     	finally{
@@ -122,6 +99,7 @@ public class Hadmyfab
     	String wallPosts = "CREATE TABLE WALLPOSTS (id INT UNSIGNED PRIMARY KEY, user_id INT REFERENCES USERS(id), body VARCHAR(200), timestamp VARCHAR(100) )";
     	String comments = "CREATE TABLE COMMENTS (id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, user_id INT REFERENCES USERS(id), wall_id INT REFERENCES WALLPOSTS(id), body VARCHAR(200), timestamp VARCHAR(100) )";
     	String aggregate = "CREATE TABLE AGGREGATE (id INT UNSIGNED PRIMARY KEY, aggregate VARCHAR(1000))";
+    	String result = "CREATE TABLE RESULT(id INT UNSIGNED,word VARCHAR(100),count INT, PRIMARY KEY(id,word))";
     	try{
     		create.executeUpdate(users);
     		create.executeUpdate(userProfile);
@@ -129,6 +107,7 @@ public class Hadmyfab
     		create.executeUpdate(wallPosts);
     		create.executeUpdate(comments);
     		create.executeUpdate(aggregate);
+    		create.executeUpdate(result);
     		System.out.println("Tables created");
     	}
     	finally{
@@ -292,102 +271,7 @@ public class Hadmyfab
     	
     }
     
-    /**Holds a tuple for a user as <userid, aggregate>**/
-    static class AggregateRecord implements Writable, DBWritable{
-    	String user_id;
-    	String aggregate;
-    	
-    	public void readFields(DataInput in) throws IOException {
-    		this.user_id = Text.readString(in);
-    		this.aggregate = Text.readString(in);
-    	}
-    	
-    	public void write(DataOutput out) throws IOException {
-    		Text.writeString(out, user_id);
-    		Text.writeString(out, aggregate);
-    	}
-    	
-        public void readFields(ResultSet resultSet) throws SQLException {
-        	this.user_id = resultSet.getString("id");
-        	this.aggregate = resultSet.getString("aggregate"); 
-        }
-        
-        public void write(PreparedStatement statement) throws SQLException{
-        	statement.setString(1, user_id);
-            statement.setString(2, aggregate);
-        }
-    }
     
-    /**Holds a final output tuple as <userid,word,count> for each user**/
-    static class AggregateWordCount implements Writable, DBWritable{
-    	String user_id;
-    	String word;
-    	int count;
-    	public AggregateWordCount(String user_id, String word, int count) {
-    	      this.user_id = user_id;
-    	      this.word = word;
-    	      this.count = count;
-    	 }
-    	    
-    	public void readFields(DataInput in) throws IOException {
-    	      this.user_id = Text.readString(in);
-    	      this.word = Text.readString(in);
-    	      this.count = in.readInt();
-    	}
-    	
-    	public void write(DataOutput out) throws IOException {
-    	      Text.writeString(out, user_id);
-    	      Text.writeString(out, word);
-    	      out.writeInt(count);
-    	}
-    	public void readFields(ResultSet resultSet) throws SQLException {
-    	      this.user_id = resultSet.getString("id");
-    	      this.word = resultSet.getString("word");
-    	      this.count = resultSet.getInt("count");    	    
-    	}
-    	
-    	public void write(PreparedStatement statement) throws SQLException {
-    	      statement.setString(1, user_id);
-    	      statement.setString(2, word);
-    	      statement.setInt(3, count);
-    	}
-    	public String toString() {
-    	      return user_id+ " " + word+" "+count;
-    	}
-    }
-    
-    //Trying to use the old api as i found lot of examples using the same, so for confirmation that the code is working
-    @SuppressWarnings("deprecation")
-	static class AggregateMapper extends MapReduceBase 
-    implements Mapper<LongWritable, AggregateRecord, Text, LongWritable> {
-    	LongWritable ONE = new LongWritable(1L);
-        private Text word = new Text();
-        
-        public void map(LongWritable key, AggregateRecord value, OutputCollector< Text, LongWritable> output, Reporter reporter)
-            throws IOException {
-        	StringTokenizer itr = new StringTokenizer(value.aggregate);
-        	while(itr.hasMoreElements()){
-        		word.set(itr.nextToken());
-        		output.collect(new Text(value.user_id+"#"+word),ONE);
-        	}
-        }
-    }
-    @SuppressWarnings("deprecation")
-    /**This should count the number of words for each user **/
-    static class AggregateReducer extends MapReduceBase 
-    implements Reducer<Text, LongWritable, AggregateWordCount, NullWritable>{
-    	
-    	NullWritable n = NullWritable.get();
-    	
-    	public void reduce(Text key, Iterator<LongWritable> values, OutputCollector<AggregateWordCount, NullWritable> output, Reporter reporter) throws IOException{
-    		int sum = 0;
-    	    while(values.hasNext()) {
-    	    	sum += values.next().get();
-    	    }
-    	    String[] temp = key.toString().split("#");
-    	    output.collect(new AggregateWordCount(temp[0],temp[1],sum),n);
-    	}
-    }
     	
     public static void main (String[] args)
     {
---
+++ MapredJob.java
@@ -0,0 +1,154 @@
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.Reducer;
+//import org.apache.hadoop.mapreduce.Mapper;
+//import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+//import org.apache.hadoop.mapred.jobcontrol.Job;
+import org.apache.hadoop.mapred.lib.db.DBConfiguration;
+import org.apache.hadoop.mapred.lib.db.DBInputFormat;
+import org.apache.hadoop.mapred.lib.db.DBOutputFormat;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+
+
+
+@SuppressWarnings("deprecation")
+public class MapredJob{
+	/**Holds a tuple for a user as <userid, aggregate>**/
+    static class AggregateRecord implements Writable, DBWritable{
+    	String user_id;
+    	String aggregate;
+    	
+    	public void readFields(DataInput in) throws IOException {
+    		this.user_id = Text.readString(in);
+    		this.aggregate = Text.readString(in);
+    	}
+    	
+    	public void write(DataOutput out) throws IOException {
+    		Text.writeString(out, user_id);
+    		Text.writeString(out, aggregate);
+    	}
+    	
+        public void readFields(ResultSet resultSet) throws SQLException {
+        	this.user_id = resultSet.getString("id");
+        	this.aggregate = resultSet.getString("aggregate"); 
+        }
+        
+        public void write(PreparedStatement statement) throws SQLException{
+        	statement.setString(1, user_id);
+            statement.setString(2, aggregate);
+        }
+    }
+    
+    /**Holds a final output tuple as <userid,word,count> for each user**/
+    static class AggregateWordCount implements Writable, DBWritable{
+    	String user_id;
+    	String word;
+    	int count;
+    	public AggregateWordCount(String user_id, String word, int count) {
+    	      this.user_id = user_id;
+    	      this.word = word;
+    	      this.count = count;
+    	 }
+    	    
+    	public void readFields(DataInput in) throws IOException {
+    	      this.user_id = Text.readString(in);
+    	      this.word = Text.readString(in);
+    	      this.count = in.readInt();
+    	}
+    	
+    	public void write(DataOutput out) throws IOException {
+    	      Text.writeString(out, user_id);
+    	      Text.writeString(out, word);
+    	      out.writeInt(count);
+    	}
+    	public void readFields(ResultSet resultSet) throws SQLException {
+    	      this.user_id = resultSet.getString("id");
+    	      this.word = resultSet.getString("word");
+    	      this.count = resultSet.getInt("count");    	    
+    	}
+    	
+    	public void write(PreparedStatement statement) throws SQLException {
+    	      statement.setString(1, user_id);
+    	      statement.setString(2, word);
+    	      statement.setInt(3, count);
+    	}
+    	public String toString() {
+    	      return user_id+ " " + word+" "+count;
+    	}
+    }
+    
+    //Trying to use the old api as i found lot of examples using the same, so for confirmation that the code is working
+	
+	public static class AggregateMapper extends MapReduceBase implements Mapper<LongWritable, AggregateRecord, Text, LongWritable> {
+    	LongWritable ONE = new LongWritable(1L);
+        private Text word = new Text();
+        
+        public void map(LongWritable key, AggregateRecord value, OutputCollector< Text, LongWritable> output, Reporter reporter)
+            throws IOException {
+        	StringTokenizer itr = new StringTokenizer(value.aggregate);
+        	while(itr.hasMoreElements()){
+        		word.set(itr.nextToken());
+        		output.collect(new Text(value.user_id+"#"+word),ONE);
+        	}
+        }
+    }
+    /**This should count the number of words for each user **/
+    
+	public static class AggregateReducer extends MapReduceBase implements Reducer<Text, LongWritable, AggregateWordCount, NullWritable>{
+    	
+    	NullWritable n = NullWritable.get();
+    	
+    	public void reduce(Text key, Iterator<LongWritable> values, OutputCollector<AggregateWordCount, NullWritable> output, Reporter reporter) throws IOException{
+    		int sum = 0;
+    	    while(values.hasNext()) {
+    	    	sum += values.next().get();
+    	    }
+    	    String[] temp = key.toString().split("#");
+    	    output.collect(new AggregateWordCount(temp[0],temp[1],sum),n);
+    	}
+    }
+    
+    
+	public static void main(String[] args) throws Exception{
+    	String[] fields = {"id","word","count"};
+    	JobConf jobconf = new JobConf(new Configuration(),Hadmyfab.class);
+    	jobconf.setJobName("Hadmyfab Mapred Job");
+    	jobconf.setMapperClass(AggregateMapper.class);
+    	jobconf.setReducerClass(AggregateReducer.class);
+    	jobconf.setCombinerClass(AggregateReducer.class);
+    	DBConfiguration.configureDB(jobconf, "com.mysql.jdbc.Driver", "jdbc:mysql://localhost/junkdb","haduser","pass");
+    	DBInputFormat.setInput(jobconf, AggregateRecord.class, "Select * from AGGREGATE;", "Select COUNT(*) from AGGREGATE");
+    	DBOutputFormat.setOutput(jobconf, "RESULT",fields);
+    	jobconf.setMapOutputKeyClass(Text.class);
+    	jobconf.setMapOutputValueClass(LongWritable.class);
+    	jobconf.setOutputKeyClass(AggregateWordCount.class);
+        jobconf.setOutputValueClass(NullWritable.class);
+        
+        try{
+        	JobClient.runJob(jobconf);
+        	
+        }
+        catch(Exception e){
+        	System.err.println (e);
+        }
+    	
+    }
+}
